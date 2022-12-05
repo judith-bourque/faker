738c5354626bc20b9a2ffdbc8de4d51a253b593b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2022-12-04 23:31:55 -0500

    Add user agent

--- R/request.R
+++ R/request.R
@@ -23,7 +23,7 @@ faker_request <- function(resource, ..., quantity = 1, locale = "en_US", seed = 
   httr2::request("https://fakerapi.it/api/v1") %>%
     httr2::req_url_path_append(resource) %>%
     httr2::req_url_query(!!!params) %>%
-    httr2::req_user_agent("my_package_name (http://my.package.web.site)") %>%
+    httr2::req_user_agent("faker (https://github.com/judith-bourque/faker)") %>%
     httr2::req_perform() %>%
     httr2::resp_body_json()
 }
